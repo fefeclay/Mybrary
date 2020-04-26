c5fb38596a32fcd29c526cb283511baf808f5dc0
Felisha Clay <felishaclay@fefeclay.local>
2020-04-26 15:14:13 -0500

    Initial Author Routes

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').parse()
-// }
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/author')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+  limit: '10mb',
+  extended: false
+}))
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://fefeclay:test1234@cluster0-qzxuy.mongodb.net/mybrary?retryWrites=true&w=majority', {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
@@ -24,5 +28,6 @@ db.on('error', error => console.log(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
